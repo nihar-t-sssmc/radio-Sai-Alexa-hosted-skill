f32c3f39bbb6427859a2e80c6360e68ab805dfc3
prasadcln <prasadcln@gmail.com>
2022-05-23 21:38:19 -0400

    Create audioAssets.js

---
+++ lambda/custom/audioAssets.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var audioData = new Map();
+
+audioData.set('BhajanStream', 'https://stream.sssmediacentre.org:8443/bhajan');
+audioData.set('DiscourseStream', 'https://stream.sssmediacentre.org:8443/discourse');
+audioData.set('AsiaStream', 'https://stream.sssmediacentre.org:8443/asia');
+audioData.set('AmeriStream', 'https://stream.sssmediacentre.org:8443/ameri');
+audioData.set('AfriStream', 'https://stream.sssmediacentre.org:8443/afri');
+audioData.set('TeluguStream', 'https://stream.sssmediacentre.org:8443/telugu');
+
+module.exports = audioData;
